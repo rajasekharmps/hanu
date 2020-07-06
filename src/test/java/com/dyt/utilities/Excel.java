c097335110c7ae63325f233ebf5d0b2ac44a98cd
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-07-06 09:50:10 +0530

    Excel.java updated

--- src/test/java/com/dyt/utilities/Excel.java
+++ src/test/java/com/dyt/utilities/Excel.java
@@ -1,5 +1,72 @@
 package com.dyt.utilities;
 
-public class Excel {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+	
+	public static FileInputStream inputstream = null;
+	public static XSSFWorkbook workbook = null;
+	
+	//get row count of a sheet
+	public static int getRowCount(String filePath, String sheetName) throws IOException
+	{
+		int rowCount;
+		File file = new File(filePath);
+		inputstream = new FileInputStream(file);
+		workbook = new XSSFWorkbook(inputstream);
+		XSSFSheet worksheet = workbook.getSheet(sheetName);
+		rowCount = worksheet.getLastRowNum()+1;		
+		return rowCount;
+	}	
+	//----------------------------------------
+	//get column count of a row
+	public static int getColumnCount(String filePath, String sheetName, int rowNumber) throws IOException
+	{
+		int colCount;
+		File file = new File(filePath);
+		inputstream = new FileInputStream(file);
+		workbook = new XSSFWorkbook(inputstream);
+		XSSFSheet worksheet = workbook.getSheet(sheetName);
+		Row row = worksheet.getRow(rowNumber-1);
+		colCount = row.getLastCellNum();		
+		return colCount;
+	}
+	//---------------------------------
+	//get cell data
+	public static String getCellData(String filePath, String sheetName, int rowNumber, int colNumber) throws IOException
+	{
+		String cellText;
+		File file = new File(filePath);
+		inputstream = new FileInputStream(file);
+		workbook = new XSSFWorkbook(inputstream);
+		XSSFSheet worksheet = workbook.getSheet(sheetName);
+		Row row = worksheet.getRow(rowNumber-1);
+		cellText = row.getCell(colNumber).getStringCellValue();		
+		return cellText;
+	}	
+	//---------------------------------
+	//get row data	
+	public static String[] getRowData(String filePath, String sheetName, int rowNumber) throws IOException
+	{		
+		String[] rowData;
+		File file = new File(filePath);
+		inputstream = new FileInputStream(file);
+		workbook = new XSSFWorkbook(inputstream);
+		XSSFSheet worksheet = workbook.getSheet(sheetName);
+		Row row = worksheet.getRow(rowNumber-1);
+		int colCount = row.getLastCellNum();
+		rowData = new String[colCount];
+		for(int iCol=0; iCol<colCount; iCol++)
+		{
+			rowData[iCol] = row.getCell(iCol).getStringCellValue();	
+			//System.out.println(row.getCell(iCol).getStringCellValue());
+		}				
+		return rowData;
+	}
 }
